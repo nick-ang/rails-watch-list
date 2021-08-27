d3a8c1a774dd81273684b47694549ab6a75208f6
Nick Ang <nicholas_ang@outlook.com>
2021-08-28 01:53:48 +1000

    fix

--- config/initializers/cloudinary.rb
+++
@@ -1,7 +0,0 @@
-Cloudinary.config do |config|
-  config.cloud_name = "sample"
-  config.api_key = "656735182639465"
-  config.api_secret = "hwYr8T_gH2wQE6Oh_wKVA6pC4gg"
-  config.secure = true
-  config.cdn_subdomain = true
-end
