320369083647d79d85b840376fdf3e582dfa4645
Nick Ang <nicholas_ang@outlook.com>
2021-08-28 00:55:08 +1000

    make beast

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,8 @@
 class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
+  has_one_attached :photo
+  validates :photo, presence: true
   validates :name, presence: true, uniqueness: true
   # , :dependent => :destroy
 end
